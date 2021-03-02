a9c00992fa8393a6cc22c9ea5f33d2a71adbbf52
Yeray Rodriguez <yeray.rodriguez@edataconsulting.de>
2021-03-02 14:10:49 +0000

    Add preliminary application code

---
+++ 15 Universe Facts/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  15 Universe Facts
+//
+//  Created by Yeray on 02/03/2021.
+//  Copyright © 2021 Hyve. All rights reserved.
+//
+
+import UIKit
+import BokenEngine
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let sceneManager = try! SceneManager()
+        sceneManager.setRootView(self.view)
+        try! sceneManager.loadTitleScene()
+    }
+
+
+}
+
