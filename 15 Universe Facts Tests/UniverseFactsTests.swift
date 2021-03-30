6dea1bcb025d74492699ba7e13d07abf2e111de4
Yeray Rodriguez <yeray.rodriguez@edataconsulting.de>
2021-03-30 11:32:00 +0100

    Change class naming

--- 15 Universe Facts Tests/UniverseFactsTests.swift
+++ 15 Universe Facts Tests/UniverseFactsTests.swift
@@ -8,7 +8,7 @@
 
 import XCTest
 
-class _5_Universe_Facts_Tests: XCTestCase {
+class UniverseFactsTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
