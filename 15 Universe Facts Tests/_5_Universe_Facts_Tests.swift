3eedf3013212a70e9b6baf476036296fd320d8b8
Yeray Rodriguez <yeray.rodriguez@edataconsulting.de>
2021-03-30 09:35:00 +0100

    Add placeholder test

---
+++ 15 Universe Facts Tests/_5_Universe_Facts_Tests.swift
@@ -0,0 +1,27 @@
+//
+//  _5_Universe_Facts_Tests.swift
+//  15 Universe Facts Tests
+//
+//  Created by Yeray on 30/03/2021.
+//  Copyright © 2021 Hyve. All rights reserved.
+//
+
+import XCTest
+
+class _5_Universe_Facts_Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(true, "Test ok")
+    }
+
+}
